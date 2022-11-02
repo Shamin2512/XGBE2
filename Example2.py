3990b50f66d185bb0ceb507ce4867fc5168e5ed9
Shamin2512 <shamin.tahasildar@gmail.com>
2022-11-02 17:17:50 +0000

    K fold validation to improve weights during training

--- Example2.py
+++ Example2.py
@@ -1,15 +1,10 @@
 # Example 2 is inbalanced data set; ~2200 in PD and ~1100 in SNP. Data must be correctly split into training and testing
 # Goal is to predict if protein is SNP or PD
 
-# **import XGBoost and other ML modules**
 import pandas as pd  # import data for training, encoding and testing
 import numpy as np  # calc the mean and SD
-#import xgboost as xgb  # XGboost Learning API
 import matplotlib.pyplot as plt  # graphing/plotting stuff
 import random as rd
-
-#from xgboost import XGBClassifier  # SK learn API for XGB model building
-#from xgboost import XGBRegressor  # SK learn API for XGB regression
 
 from sklearn.metrics import (
     matthews_corrcoef,  # MCC for evaluation
@@ -22,6 +17,7 @@ from sklearn.metrics import (
 from sklearn.model_selection import (
     train_test_split,  # Splits data frame into the training set and testing set
     GridSearchCV,  # Cross validation to improve hyperparameters
+    StratifiedKFold
     )
 from sklearn.ensemble import RandomForestClassifier #SK learn API
 
@@ -30,6 +26,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 
 from sklearn.utils import shuffle #shuffles rows
+import seaborn as sns
 
 # **Create, clean and convert the train.csv dataset to a dataframe**
 df = pd.read_csv('E2.csv')  # Pandas creates data frame from the .csv mutation data
@@ -42,6 +39,7 @@ df.reset_index(inplace = True)
 X = df.drop('dataset', axis =1).fillna('0')
 y_encoded = pd.get_dummies(df, columns=['dataset'])
 y = y_encoded['dataset_pd'].copy().astype('int32')
+print("Total samples:", len(df))
 print("Number of PD:", len(df.loc[df['dataset'] == 'pd']))
 print("Number of SNP:", len(df.loc[df['dataset'] == 'snp']))
 
@@ -53,16 +51,47 @@ StandardScaler().fit(X).transform(X)
 #**Parameters pipeline**
 pipeline = make_pipeline( #equivilant of fitting to XGB parameters
     StandardScaler(),
-    LogisticRegression(solver='saga', max_iter=5000)
+    LogisticRegression(solver='saga', max_iter=2000, class_weight = [{0:8557286432160804, 1:1.0-8557286432160804}])
     )
 # **Split data into training and test**
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.8, random_state=42, stratify=y)
 
 pipeline.fit(X_train, y_train)
+weights = np.linspace(0.0,0.99,200) # creates 200 evenly spaced values between 0 and 0.99
+param_grid = {'class_weight': [{0:x, 1:1.0-x} for x in weights]} # applys weights to each value between 0 and 0.99
 
-print(pipeline.fit)
-print(accuracy_score(pipeline.predict(X_test), y_test))
+gridsearch = GridSearchCV(
+    estimator = LogisticRegression(solver='saga'),
+    param_grid = param_grid,
+    cv = StratifiedKFold(),
+    n_jobs = -1,
+    scoring = 'f1',
+    verbose = 2,
+    ).fit(X_train, y_train)
+
+#Ploting the score for different values of weight
+plt.figure(figsize=(12,8))
+sns.set_style('whitegrid')
+weigh_data = pd.DataFrame({ 'score': gridsearch.cv_results_['mean_test_score'], 'weight': (1- weights)})
+sns.lineplot(x = weigh_data['weight'], y =  weigh_data['score'])
+plt.xlabel('Weight for class 1')
+plt.ylabel('F1 score')
+plt.xticks([round(i/10,1) for i in range(0,11,1)])
+plt.title('Scoring for different class weights', fontsize=24)
+
+
+print(gridsearch)
+    
+                                   
+
+
+y_pred = clf.predict(X_test)
+print("Accuracy:\n", accuracy_score(pipeline.predict(X_test), y_test))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
 print("MCC:\n", matthews_corrcoef(y_test, y_pred))
+print("F1:\n", f1_score(y_test, y_pred))
+
+plt.show()
 
 #    'booster': 'gbtree',  # non-linear, tree method (default)
 #    'verbosity': 1,  # outputs the evaluation of each tree
@@ -73,26 +102,3 @@ print("MCC:\n", matthews_corrcoef(y_test, y_pred))
 #    'min_child_weight': 1,
 #    'objective': 'binary:logistic'  # classifies the outcome as either 0 (SNP), or 1 (PD). Non multiclass classification
 #}
-
-
-# **Plot confusion matrix using the true and predicted values**
-#clf = xgb.XGBClassifier(**param)
-#clf.fit(d_train1, y_test1)
-#y_pred = clf.predict(X_test)
-#ConfusionMatrixDisplay.from_predictions(y_test, y_pred)
-
-y_pred1 = model1.predict(d_test1)  # No longer a pandas DF, is now a numpy array as Dmatrix
-y_pred2 = model2.predict(d_test2)  # No longer a pandas DF, is now a numpy array as Dmatrix
-y_pred3 = model3.predict(d_test3)  # No longer a pandas DF, is now a numpy array as Dmatrix
-cm1 = confusion_matrix(y_test1, y_pred1 > 0.5)
-cm2 = confusion_matrix(y_test2, y_pred2 > 0.5)
-cm3 = confusion_matrix(y_test3, y_pred3 > 0.5)
-
-print("Confusion Matrix:\n", cm1)
-print("Confusion Matrix:\n", cm2)
-print("Confusion Matrix:\n", cm3)
-
-print("MCC:\n", matthews_corrcoef(y_test1, y_pred1 >0.5), matthews_corrcoef(y_test2, y_pred2 > 0.5),
-      matthews_corrcoef(y_test2, y_pred3 > 0.5))
-print("F1 Score:\n", f1_score(y_test3, y_pred1 > 0.5), f1_score(y_test3, y_pred3 > 0.5),
-      f1_score(y_test3, y_pred3 > 0.5))
